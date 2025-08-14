c395e59eab7eb32b2967ecaa9ea26517e147e0ad
dpqlsn <a01037111209@gmail.com>
2025-08-14 11:45:04 +0900

    feat(#9) :: 팀스페이스 팀추가

--- src/admin/page/Teamspace/Teamspace.tsx
+++ src/admin/page/Teamspace/Teamspace.tsx
@@ -1,17 +1,24 @@
-import * as _ from './style';
-import '@_styles'
-import NavBar from '@_all/component/sibebar/sidebar';
-import Team from '@_components/Teamspace/Team';
+import * as _ from "./style";
+import "@_styles";
+import NavBar from "@_all/component/sibebar/sidebar";
+import Team from "@_components/Teamspace/Team";
+import Plus from "@_all/assets/plus.svg";
 
-export default function Teamspace(){
-    return(
-        <_.Container>
+export default function Teamspace() {
+    return (
+        <_.Wrapper>
             <NavBar />
-            <_.Wrapper>
+        <_.Header>
+            <_.TitleBox>
                 <_.Title>동아리 팀원들을 배치해요</_.Title>
                 <_.Subtitle>각 동아리 별로 팀원들을 배치하고 관리해요</_.Subtitle>
-                <Team />
-            </_.Wrapper>
-        </_.Container>
-    )
+            </_.TitleBox>
+            <_.Group>
+                <_.Img src={Plus} alt="팀원추가" />
+                <_.Plus>팀원추가</_.Plus>
+            </_.Group>
+        </_.Header>
+        <Team />
+        </_.Wrapper>
+    );
 }
--- src/admin/page/Teamspace/style.ts
+++ src/admin/page/Teamspace/style.ts
@@ -9,20 +9,48 @@ export const Wrapper = styled.div`
     margin-left: 14.5%;
 `;
 
-export const Title = styled.div`
+export const Header = styled.div`
     display: flex;
-    justify-content: left;
+    justify-content: space-between;
+    align-items: flex-start;
+    margin-top: 2%;
+    margin-bottom: 6%;
+`;
+
+export const TitleBox = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Title = styled.div`
     font-size: 1.5rem;
     font-weight: 600;
     color: #1D1D1D;
-    margin-bottom: 1%;
-    margin-top: 2%;
+    margin-bottom: 0.5rem;
 `;
 
 export const Subtitle = styled.div`
-    display: flex;
-    justify-content: left;
     font-size: 1.2rem;
     color: #707070;
-    margin-bottom: 6%;
+`;
+
+export const Group = styled.button`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: transparent;
+    margin-right: 8%;
+    border: none;
+    cursor: pointer;
+`;
+
+export const Img = styled.img`
+    width: 80%;
+    margin-bottom: 0.4rem;
+`;
+
+export const Plus = styled.span`
+    font-size: 0.9rem;
+    color: #909090;
+    font-family: 'Paperlogy';
 `;
