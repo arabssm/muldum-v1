b84d87728568f8a22bf2cd57058d6995c0c042cc
dpqlsn <a01037111209@gmail.com>
2025-08-25 01:12:25 +0900

    fix(#19) :: 유동적인 에러페이지

--- src/admin/components/NotFound/NotFound.tsx
+++ src/admin/components/NotFound/NotFound.tsx
@@ -1,16 +1,22 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import * as _ from './style';
-import '@_styles';
 import NavBar from '@_all/component/sibebar/sidebar';
+import type { LocationState } from './types';
 
 export default function Notfound() {
-const navigate = useNavigate();
-return(
-    <_.Container>
+    const navigate = useNavigate();
+    const location = useLocation();
+    const state = location.state as LocationState;
+
+    const errorCode = state?.errorCode || 404;
+    const message = state?.message || '요청하신 화면을 찾을 수 없습니다';
+
+    return (
+        <_.Container>
         <NavBar />
-        <_.Error>404</_.Error>
-        <_.Title>요청하신 화면을 찾을 수 없습니다</_.Title>
-        <_.SubTitle onClick={() => navigate(-1)}>이전 페이지로 돌아가기</_.SubTitle>
-    </_.Container>
-);
+            <_.Error>{errorCode}</_.Error>
+            <_.Title>{message}</_.Title>
+            <_.SubTitle onClick={() => navigate(-1)}>이전 페이지로 돌아가기</_.SubTitle>
+        </_.Container>
+    );
 }
