5495ff875c82d8f78e93b5f2fa13bc94686b0ca5
dpqlsn <a01037111209@gmail.com>
2025-09-08 20:19:29 +0900

    fix(#24) :: 무식하지만 간단한 방법으로,,,,,,,,

--- src/all/component/sibebar/IconMenu.tsx
+++ src/all/component/sibebar/IconMenu.tsx
@@ -8,6 +8,6 @@ export const IconMenu = [
     { label: '물품관리', path: ['/project-choice','/object/all','/object','/project-approval'], icon: icons.item, iconActive: icons.itemActive },
     { label: '월말평가', path: ['/evaluate'], icon: icons.alarm, iconActive: icons.alarmActive },
     { label: '공지사항', path: ['/notice','/create-notice','/notice/edit','notice/'], icon: icons.notice, iconActive: icons.noticeActive },
-    { label: '팀스페이스', path: ['/team-space', '/club/1'], icon: icons.teamspace, iconActive: icons.teamspaceActive },
+    { label: '팀스페이스', path: ['/team-space', '/club/1', '/club/2', '/club/3', '/club/4', '/club/5', '/club/6', '/club/7', '/club/8', '/club/9', '/club/10', '/club/11', '/club/12'], icon: icons.teamspace, iconActive: icons.teamspaceActive },
     { label: '설정', path: ['/setting'], icon: icons.setting, iconActive: icons.settingActive }
 ];
