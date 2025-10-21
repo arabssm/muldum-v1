3265bc20d6de88ae1bb01e1f047ed361bcaff46c
bitbyte08 <me@bitworkspace.kr>
2025-10-22 00:45:42 +0900

    feat :: NavBar fixed에서 flex로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Main from '@_main/Main';
-import Notice from '@_notice/Notice';
-import DetailNotice from '@_notice/Detail/DetailNotice';
-import CreateNotice from '@_pages/Notice/Create/CreateNotice';
-import NotFound from '@_components/NotFound/NotFound';
-import NoticeEdit from '@_pages/Notice/Edit/NoticeEdit';
-import ProjectChoice from '@_pages/Item/Choice/ProjectChoice';
-import Approval from '@_pages/Item/Approval/Approval';
-import Teamspace from '@_pages/Teamspace/Teamspace';
-import ClubHistory from '@_pages/ClubHistory/ClubHistory';
-import Object from '@_page/object/object';
-import All from '@_page/object/all';
-import Selectwhatteam from '@_all/pages/selectwhatteam'
 import { useLoginModalStore } from './atom/Modal';
 import { useLoadingStore } from './atom/Loading';
 import LoginModal from './all/component/modal/login/login';
-import GoogleLogin from '@_all/pages/GogleLogin';
 import Loading from './all/component/loading/loading';
-import TeamDetail from '@_all/component/team/TeamDetail/TeamDetail';
-import TeamEdit from '@_page/TeamEdit/TeamEdit';
-import AuthConfirm from '@_all/auth/auth';
 import ScreenSizeWarning from './all/component/ScreenSizeWarning/ScreenSizeWarning';
 import AppRoutes from "./AppRoutes";
 import NavBar from "@_navbar/sidebar";
+import styled from "@emotion/styled";
+
+const MainContainer = styled.main`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+`
+
+const ContentContainer = styled.div`
+    flex: 1;
+    margin-left: 13%;
+    padding: 2rem 3rem 2rem 1rem;
+    box-sizing: border-box;
+    overflow: auto;
+    
+    /* 큰 노트북 (1440px 이상) */
+    @media (min-width: 1440px) {
+        padding: 2rem 4rem 2rem 1.5rem;
+    }
+    
+    /* 일반 노트북 (1200px - 1439px) */
+    @media (max-width: 1439px) and (min-width: 1200px) {
+        padding: 2rem 2.5rem 2rem 1.2rem;
+    }
+    
+    /* 작은 노트북 (1024px - 1199px) */
+    @media (max-width: 1199px) and (min-width: 1024px) {
+        padding: 1.5rem 2rem 1.5rem 1rem;
+    }
+    
+    /* 매우 작은 노트북 (900px - 1023px) */
+    @media (max-width: 1023px) and (min-width: 900px) {
+        padding: 1.5rem 1.5rem 1.5rem 0.8rem;
+    }
+`;
 
 export default function App() {
   const { isOpen } = useLoginModalStore();
@@ -49,10 +68,12 @@ export default function App() {
     return <ScreenSizeWarning />;
   }
   return (
-    <>
+    <MainContainer>
       <NavBar />
-      <AppRoutes />
+      <ContentContainer>
+        <AppRoutes />
+      </ContentContainer>
       {isOpen && <LoginModal />}
-    </>
+    </MainContainer>
   );
 }
