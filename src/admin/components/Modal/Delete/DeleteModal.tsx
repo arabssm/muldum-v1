d22359997816da34b7530a8597520bd0debe32f4
123isi <123isi@naver.com>
2025-10-23 11:48:09 +0900

    fix :: 공지수정

--- src/admin/components/Modal/Delete/DeleteModal.tsx
+++ src/admin/components/Modal/Delete/DeleteModal.tsx
@@ -8,7 +8,7 @@ export default function DeleteModal({ onCancel, onConfirm }: Props) {
     <_.Container>
       <_.Modal>
         <_.TextArea>
-          <_.Title>공지사항을을(를) 정말 삭제하시겠습니까?</_.Title>
+          <_.Title>공지사항을(를) 정말 삭제하시겠습니까?</_.Title>
           <_.SubTitle>삭제한 내용은 되돌릴 수 없습니다.</_.SubTitle>
         </_.TextArea>
         <_.ButtonGroup>
