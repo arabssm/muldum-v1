ade848c9d23a2929f1bed895ce6baa3d261ac405
123isi <123isi@naver.com>
2025-10-23 11:46:56 +0900

    fix :: 공지수정

--- src/admin/components/Modal/Delete/DeleteModal.tsx
+++ src/admin/components/Modal/Delete/DeleteModal.tsx
@@ -2,23 +2,13 @@ import * as _ from './style';
 import '@_styles';
 import { Props } from './type'
 
-export default function DeleteModal({ onCancel, onConfirm, name }: Props) {
-  const formatNameForTwoLines = (text: string) => {
-    if (text.length <= 15) return text;
-
-    const midPoint = Math.ceil(text.length / 2);
-    const firstLine = text.substring(0, midPoint);
-    const secondLine = text.substring(midPoint);
-
-    return `${firstLine}\n${secondLine}`;
-  };
+export default function DeleteModal({ onCancel, onConfirm }: Props) {
 
   return (
     <_.Container>
       <_.Modal>
         <_.TextArea>
-          <_.Title>{formatNameForTwoLines(name)}</_.Title>
-          <_.SubTitle>을(를) 정말 삭제하시겠습니까?</_.SubTitle>
+          <_.Title>공지사항을을(를) 정말 삭제하시겠습니까?</_.Title>
           <_.SubTitle>삭제한 내용은 되돌릴 수 없습니다.</_.SubTitle>
         </_.TextArea>
         <_.ButtonGroup>
