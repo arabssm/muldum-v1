fec397cdb5e72c3ea8a45949a5a2ca7c12ffa6b8
123isi <123isi@naver.com>
2025-10-27 00:33:50 +0900

    fix :: modal

--- src/all/pages/selectwhatteam.tsx
+++ src/all/pages/selectwhatteam.tsx
@@ -2,11 +2,11 @@ import * as _ from './style';
 import Menu from '../component/team/menu';
 import '@_styles';
 
-export default function Selectwhatteam(){
+export default function Selectwhatteam() {
     return (
         <>
-            <_.Title>확인 할 프로젝트를 선택하세요</_.Title>
-            <_.Subtitle>어떤 팀이 있는지 조회하세요</_.Subtitle>
+            <_.Title>진행 중인 프로젝트를 확인하세요.</_.Title>
+            <_.Subtitle>참여 중인 팀 목록을 볼 수 있습니다.</_.Subtitle>
             <Menu />
         </>
     )
