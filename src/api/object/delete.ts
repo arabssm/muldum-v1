591133ee06baaa56a6d44b58b51a6de89e967554
123isi <123isi@naver.com>
2025-10-27 01:00:08 +0900

    fix :: modal

--- src/api/object/delete.ts
+++ src/api/object/delete.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../../lib/axiosInatance";
 
 export const deleteTemporaryItem = async (itemId: number) => {
   try {
-    const response = await axiosInstance.delete(`/temp/${itemId}`);
+    const response = await axiosInstance.delete(`std/temp/${itemId}`);
     return response.data;
   } catch (error) {
     console.error("임시 신청 삭제 실패:", error);
