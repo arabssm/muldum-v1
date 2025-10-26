1dfedd2625df5fcdbfe3b264c8721b65b08956df
123isi <123isi@naver.com>
2025-10-27 00:20:58 +0900

    fix :: modal

--- src/admin/page/Item/Choice/ProjectChoice.tsx
+++ src/admin/page/Item/Choice/ProjectChoice.tsx
@@ -1,12 +1,21 @@
 import * as _ from './style';
 import Menu from '@_menu/Menu';
 import '@_styles';
+import { useUserStore } from '../../../../atom/User';
 
-export default function ProjectChoice(){
+export default function ProjectChoice() {
+    const { user } = useUserStore();
+
+    const isTeacher = user?.userType === "TEACHER";
+
     return (
         <>
-            <_.Title>물품 승인 프로젝트 선택</_.Title>
-            <_.Subtitle>물품을 승인할 프로젝트를 선택해주세요</_.Subtitle>
+            <_.Title>
+                {isTeacher ? "물품 승인 프로젝트 선택" : "물품 신청 프로젝트 선택"}
+            </_.Title>
+            <_.Subtitle>
+                {isTeacher ? "물품을 승인할 프로젝트를 선택해주세요" : "물품을 신청할 프로젝트를 선택해주세요"}
+            </_.Subtitle>
             <Menu />
         </>
     )
