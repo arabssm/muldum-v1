0a706152cd0c380221d2ae4f826c0aa9e8150bea
dpqlsn <a01037111209@gmail.com>
2025-10-28 19:10:35 +0900

    fix :: 팀스페이스 보러가기 경로 수정

--- src/all/component/menu/items.ts
+++ src/all/component/menu/items.ts
@@ -2,6 +2,6 @@ export const items = [
     { title: "물품 신청 하러가기", path: "/project-choice" },
     { title: "공지사항 보러가기", path: "/notice" },
     //{ title: "월말평가 작성하기", path: "/" },
-    { title: "팀스페이스 보러가기", path: "/team-space" },
+    { title: "팀스페이스 보러가기", path: "/team-space-menu" },
     { title: "역대 전공동아리 보러가기", path: "/club-history" },
 ];
